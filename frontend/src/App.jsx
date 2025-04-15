5ce46ab49065f9360b5976e21516921f1076e6e3
markvanmeerten <markvanmeerten@gmail.com>
2025-04-15 11:54:50 +0200

    fixes laptop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const apiUrl = import.meta.env.VITE_API_URL;
 
   // useEffect(() => {
@@ -18,28 +18,26 @@ function App() {
     console.log("new count: " + count);
 
     fetch(`${apiUrl}/increment`, {
-      method: 'GET'
+      method: "GET",
     })
-      .then(res => res.json())
-      .then(data => console.log('Teller:', data.value));
-  }
-                                                                                                                                                                                                                                                                      v
+      .then((res) => res.json())
+      .then((data) => console.log("Teller:", data.value));
+  };
+
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        
+
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={handleClickCount}>
-          count is {count}
-        </button>
+        <button onClick={handleClickCount}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -48,7 +46,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
