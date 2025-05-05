8811257139abcea9f29ed7d665e00497714a5c49
markvanmeerten <markvanmeerten@gmail.com>
2025-05-05 17:59:20 +0200

    tst 9999

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        if ($_ENV['APP_ENV'] === 'local') {
+        if (env('APP_ENV') === 'local') {
             // $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
         }
     })
