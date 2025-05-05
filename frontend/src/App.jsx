ac747ea827ed1e840141ca31bb090d7cf6f9dab6
markvanmeerten <markvanmeerten@gmail.com>
2025-05-05 16:36:38 +0200

    update frontpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,40 +8,23 @@ function App() {
   const apiUrl = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
-    console.log("FETCHING");
-
     fetch(`${apiUrl}/increment/1`, {
       method: "GET",
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log("Teller:", data.value);
-
         setCount(data.value);
       });
-
-    // fetch(`${apiUrl}/increment/1`, {
-    //   method: "GET",
-    // })
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     console.log("Teller:", data.value);
-
-    //     setCount(data.value);
-    //   });
   }, []);
 
   const handleClickCount = () => {
     setCount((count) => count + 1);
-    console.log("new count: " + count);
 
     fetch(`${apiUrl}/increment/1`, {
       method: "POST",
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log("Teller:", data.value);
-
         setCount(data.value);
       });
   };
@@ -60,13 +43,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={handleClickCount}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR!
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
